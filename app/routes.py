1e3de92559df295ec734028bb94cd334ea65a9c4
mattstermh93 <mattstermh@hotmail.com>
2018-06-27 14:43:20 -0400

    sign in link works from create acc page

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,15 @@ from app import app
 from flask import render_template, redirect, url_for, flash
 from app.forms import LoginForm, RegistrationForm, RegistrationForm2
 
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def register():
     register_form = RegistrationForm()
     if register_form.validate_on_submit():
-        return redirect(url_for('login'))
-    return render_template('login2.html', form=register_form)
+        return redirect(url_for('login2'))
+    return render_template('login.html', form=register_form)
 
-@app.route('/')
+
 @app.route('/login2', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
